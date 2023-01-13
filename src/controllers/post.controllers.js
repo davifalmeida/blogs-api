868efed585b0ba7166facd96322182a110a763bd
Davi <davifalmeida@gmail.com>
2023-01-13 15:32:13 -0300

    estrutura do req 12 e req 13 completo

--- src/controllers/post.controllers.js
+++ src/controllers/post.controllers.js
@@ -1,17 +1,19 @@
 // const { postMiddleware } = require('../middlewares');
 const { postService } = require('../services');
 
-const create = async (req, res) => {
-    const { title, content, categoryIds } = req.body;
-    const { id: userId } = req.user;
-    const newPost = await postService.createPost(
-      title,
-      content,
-      categoryIds,
-      userId,
-    );
-    return res.status(201).json(newPost);
-  };
+const create = async (req, res, next) => {
+    try {
+      const { title, content, categoryIds, userId } = req.body;
+      const post = await postService.createPost({ title, content, categoryIds, userId });
+      res.status(201).json({ post });
+    } catch (error) {
+        if (error.message === 'one or more "categoryIds" not found') {
+          res.status(400).json({ message: error.message });
+        } else {
+          next(error);
+        }
+      }
+    };
 
     const getAll = async (_req, res) => {
         const posts = await postService.getAllPosts();
